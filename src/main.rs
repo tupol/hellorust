5f1f61662cd26e63a5d7826f89c02877ae87476c
Willem Vermeer <willem.vermeer@gmail.com>
2023-01-08 10:41:56 +0100

    added scheduler for future health checks

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+use std::time::Duration;
+
 use serde::{Deserialize, Serialize};
-use warp::{Filter, http::StatusCode};
+use tokio_cron_scheduler::{Job, JobScheduler};
+use warp::{http::StatusCode, Filter};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     #[derive(Debug, Serialize, Deserialize)]
-    struct LogonRequest { username: String, password: String }
+    struct LogonRequest {
+        username: String,
+        password: String,
+    }
 
     async fn print_request(item: LogonRequest) -> Result<impl warp::Reply, warp::Rejection> {
         println!("{:?}", item);
@@ -26,9 +31,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Hellorust open for e-business");
 
-    warp::serve(routes)
-        .run(([127, 0, 0, 1], 3031))
-        .await;
+    let mut sched = JobScheduler::new()
+        .await
+        .expect("Scheduler starting problem");
+
+    let job = Job::new_repeated(Duration::from_secs(7), |_uuid, _l| {
+        println!("I'm repeated every 7 seconds");
+    })
+    .unwrap();
+
+    sched.add(job).await.expect("Could not add job");
+
+    sched.set_shutdown_handler(Box::new(|| {
+        Box::pin(async move {
+            println!("Scheduler shutting down done");
+        })
+    }));
+    sched.start().await.expect("`Could not start scheduler");
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3031)).await;
 
     Ok(())
 }
